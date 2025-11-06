df9e50086f4f5f4aae4f9c69c4ae0e2acf056c6d
Akamunyereka <kevinakamunyereka12@gmail.com>
2025-11-06 05:24:21 -0800

    finished

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,49 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Outer loop for "play again" functionality
+        string playAgain = "yes";
+
+        while (playAgain.ToLower() == "yes")
+        {
+            // Generate a random magic number between 1 and 100
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;  // initialize to something not equal to magicNumber
+            int guessCount = 0;  // track number of guesses
+
+            Console.WriteLine("I'm thinking of a number between 1 and 100.");
+
+        // Inner loop for guessing
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                }
+            }
+
+            Console.Write("Do you want to play again (yes/no)? ");
+            playAgain = Console.ReadLine();
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Thanks for playing! Goodbye!");
     }
 }
